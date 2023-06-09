c9579bea27f1c09d69c620974db06adec71490fa
Sam Malyarov <107198574+samalyarov@users.noreply.github.com>
2023-06-09 13:35:16 +0400

    Create table_creation.sql

---
+++ postgresql_db/table_creation.sql
@@ -0,0 +1,17 @@
+CREATE TABLE buy_orders (
+	record_id SERIAL PRIMARY KEY,
+	record_date TIMESTAMP WITH TIMEZONE,
+	item_id INT,
+	item_region INT,
+	item_system INT,
+	min_quantity INT,
+	volume INT,
+	p_weighted_average FLOAT,
+	p_average FLOAT,
+	p_variance FLOAT,
+	p_stddev FLOAT,
+	p_median FLOAT,
+	p_fivepercent FLOAT,
+	p_max FLOAT,
+	p_min FLOAT,
+	);
